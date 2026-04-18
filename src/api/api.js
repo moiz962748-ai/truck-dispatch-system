e12b07032db44e4352fe1c6db6e2f1413d8b4d19
Abdul Moiz <moiz962748@gmail.com>
2026-04-19 00:00:57 +0500

    Fix: Added /api to baseURL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://truck-dispatch-api.onrender.com',
+  baseURL: 'https://truck-dispatch-api.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
